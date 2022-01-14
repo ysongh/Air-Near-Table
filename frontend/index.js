15a7fd8dd9bf78d9690f33fa7ab10bfa4e5faadc
You Song Hou <yousonghou@gmail.com>
2022-01-14 00:30:50 -0500

    feat: update cell values for records when the table load

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,31 @@
-import {initializeBlock} from '@airtable/blocks/ui';
+import { initializeBlock, useBase, useRecords } from '@airtable/blocks/ui';
 import React from 'react';
 
 function HelloWorldApp() {
     // YOUR CODE GOES HERE
+    const base = useBase();
+    const table = base.getTableByName('Explorer');
+    const records = useRecords(table.selectRecords());
+
+    function updateRecord(record, recordFields) {
+        if (table.hasPermissionToUpdateRecord(record, recordFields)) {
+            table.updateRecordAsync(record, recordFields);
+        }
+        // The updated values will now show in your app (eg in
+        // `table.selectRecords()` result) but are still being saved to Airtable
+        // servers (e.g. other users may not be able to see them yet).
+    }
+
+    updateRecord(records[0], {
+        'Name': '1',
+        'Notes': 'Test',
+    });
+
+    updateRecord(records[1], {
+        'Name': '2',
+        'Notes': 'Test2',
+    });
+
     return <div>Hello world 🚀</div>;
 }
 
