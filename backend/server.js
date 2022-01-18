d1b256f45c9a7275e563665af01231058d61330d
You Song Hou <yousonghou@gmail.com>
2022-01-18 01:06:28 -0500

    feat: install 'near-api-js' and add a route to fetch all validators

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
+const nearAPI = require("near-api-js");
 const express = require('express');
 const app = express();
 
+const { providers } = nearAPI;
+
+// network config
+const provider = new providers.JsonRpcProvider(
+	"https://rpc.mainnet.near.org"
+);	
+  
 app.get('/', (req, res) => res.send('Server Work'));
+
+app.get('/validators', async (req, res) =>  {
+	const result = await provider.validators(null);
+  console.log("Result: ", result);
+});
 
 const port = process.env.PORT || 4000;
 
