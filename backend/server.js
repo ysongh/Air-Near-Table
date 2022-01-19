195e7b88dbb0687b1c6c465fe0b05afcc484bd60
You Song Hou <yousonghou@gmail.com>
2022-01-18 23:34:58 -0500

    refactor: update a route for validators to return data as json

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,16 @@ const { providers } = nearAPI;
 
 // network config
 const provider = new providers.JsonRpcProvider(
-	"https://rpc.mainnet.near.org"
+  "https://rpc.mainnet.near.org"
 );	
   
 app.get('/', (req, res) => res.send('Server Work'));
 
 app.get('/validators', async (req, res) =>  {
-	const result = await provider.validators(null);
-  console.log("Result: ", result);
+  const result = await provider.validators(null);
+  return res.status(200).json({
+    data: result,
+  });
 });
 
 const port = process.env.PORT || 4000;
