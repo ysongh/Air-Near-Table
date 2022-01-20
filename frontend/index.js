d4b2c49374f7496b20319bd1c0ac0b345030012c
You Song Hou <yousonghou@gmail.com>
2022-01-20 00:14:14 -0500

    fix: add table.createRecordsAsync() if a record is empty

--- frontend/index.js
+++ frontend/index.js
@@ -15,14 +15,22 @@ function HelloWorldApp() {
         const response = await fetch("http://localhost:4000/validators");
         const data = await response.json();
         console.log(data.data);
+        const validators =  data.data.current_validators;
 
-
-        data.data.current_validators.forEach((validator, index) => {
-            updateRecord(records[index], {
-                'Account ID': validator.account_id,
-                'Stake': validator.stake,
-            });
-        });
+        for(let i = 0; i < validators.length; i++){
+            if(i < records.length){
+                updateRecord(records[i], {
+                    'Account ID': validators[i].account_id,
+                    'Stake': validators[i].stake,
+                });
+            }
+            else{
+                createRecord([
+                    {fields: {'Account ID': validators[i].account_id}},
+                    {fields: {'Stake': validators[i].stake}}
+                ]);
+            }
+        }
     }
 
     function updateRecord(record, recordFields) {
@@ -34,7 +42,19 @@ function HelloWorldApp() {
         // servers (e.g. other users may not be able to see them yet).
     }
 
-    return <div>Current Validators</div>;
+    function createRecord(recordFields) {
+        if (table.hasPermissionToCreateRecord(record, recordFields)) {
+            table.createRecordsAsync([recordFields]);
+        }
+        // The updated values will now show in your app (eg in
+        // `table.selectRecords()` result) but are still being saved to Airtable
+        // servers (e.g. other users may not be able to see them yet).
+    }
+
+    return <div>
+        Current Validators
+        {records.length}
+    </div>;
 }
 
 initializeBlock(() => <HelloWorldApp />);
