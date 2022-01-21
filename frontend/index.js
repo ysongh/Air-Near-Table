c30aa6c4a496ca328285b8f9fbae753fc8dabfe9
You Song Hou <yousonghou@gmail.com>
2022-01-21 00:58:07 -0500

    fix: display a list of current validators

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,10 @@
 import { initializeBlock, useBase, useRecords } from '@airtable/blocks/ui';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function HelloWorldApp() {
     // YOUR CODE GOES HERE
+    const [validators, setValidators] = useState([]);
+
     useEffect(() => {
         getData();
     }, [])
@@ -16,49 +18,40 @@ function HelloWorldApp() {
         const data = await response.json();
         const validators = data.data;
         console.log(validators);
+        setValidators(validators);
 
         for(let i = 0; i < validators.length; i++){
-            if(i < records.length){
-                updateRecord(records[i], {
-                    'Account ID': validators[i].account_id,
-                    'Stake': validators[i].stake.split(".")[0] + " N",
-                    'Is Slashed': validators[i].is_slashed.toString(),
-                    'Expected Blocks': validators[i].num_expected_blocks,
-                    'Expected Chunks': validators[i].num_expected_chunks,
-                    'Produced Blocks': validators[i].num_produced_blocks,
-                    'Produced Chunks': validators[i].num_produced_chunks
-                });
-            }
-            else{
-                createRecord([
-                    {fields: {'Account ID': validators[i].account_id}},
-                    {fields: {'Stake': validators[i].stake.split(".")[0] + " N"}},
-                    {fields: {'Is Slashed': validators[i].is_slashed}},
-                    {fields: {'Expected Blocks': validators[i].num_expected_blocks}},
-                    {fields: {'Expected Chunks': validators[i].num_expected_chunks}},
-                    {fields: {'Produced Blocks': validators[i].num_produced_blocks}},
-                    {fields: {'Produced Chunks': validators[i].num_produced_chunks}}
-                ]);
+            if(i >= records.length){
+                await table.createRecordAsync();
             }
         }
     }
 
-    function updateRecord(record, recordFields) {
-        if (table.hasPermissionToUpdateRecord(record, recordFields)) {
-            table.updateRecordAsync(record, recordFields);
+    async function addData(){
+        for(let i = 0; i < validators.length; i++){
+            updateRecord(records[i], {
+                'Account ID': validators[i].account_id,
+                'Stake': validators[i].stake.split(".")[0] + " N",
+                'Is Slashed': validators[i].is_slashed.toString(),
+                'Expected Blocks': validators[i].num_expected_blocks,
+                'Expected Chunks': validators[i].num_expected_chunks,
+                'Produced Blocks': validators[i].num_produced_blocks,
+                'Produced Chunks': validators[i].num_produced_chunks
+            });
         }
+    }
+
+    async function updateRecord(record, recordFields) {
+        await table.updateRecordAsync(record, recordFields);
         // The updated values will now show in your app (eg in
         // `table.selectRecords()` result) but are still being saved to Airtable
         // servers (e.g. other users may not be able to see them yet).
     }
 
-    function createRecord(recordFields) {
-        table.createRecordsAsync(recordFields);
-    }
-
     return <div>
         Current Validators
         {records.length}
+        <button onClick={addData}>Show</button>
     </div>;
 }
 
