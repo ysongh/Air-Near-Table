bc6c14ff08fade8c6e103fe6bfc0369d9d99f1f0
You Song Hou <yousonghou@gmail.com>
2022-01-24 01:54:35 -0500

    feat: use useWatchable() React hook for watching data in Airtable models

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,13 @@
-import { initializeBlock, useBase, useRecords } from '@airtable/blocks/ui';
+import { initializeBlock, useBase, useRecords, useLoadable, useWatchable } from '@airtable/blocks/ui';
 import { Box, Button, Loader } from '@airtable/blocks/ui';
+import {cursor} from '@airtable/blocks';
 import React, { useEffect, useState } from 'react';
 
-function AirNearTable({cursor}) {
+function AirNearTable() {
     // YOUR CODE GOES HERE
     const [validators, setValidators] = useState([]);
+    const [selectedRecordId, setSelectedRecordId] = useState(null);
+    const [selectedFieldId, setSelectedFieldId] = useState(null);
     const [loading, setLoading] = useState(false);
 
     const base = useBase();
@@ -16,6 +19,25 @@ function AirNearTable({cursor}) {
     }
 
     const records = useRecords(table.selectRecords());
+
+    useLoadable(cursor);
+
+    useWatchable(cursor, ['selectedRecordIds', 'selectedFieldIds'], () => {
+        // If the update was triggered by a record being de-selected,
+        // the current selectedRecordId will be retained.  This is
+        // what enables the caching described above.
+        if (cursor.selectedRecordIds.length > 0) {
+            // There might be multiple selected records. We'll use the first
+            // one.
+            setSelectedRecordId(cursor.selectedRecordIds[0]);
+        }
+        if (cursor.selectedFieldIds.length > 0) {
+            // There might be multiple selected fields. We'll use the first
+            // one.
+            setSelectedFieldId(cursor.selectedFieldIds[0]);
+        }
+        console.log(cursor);
+    });
 
     useEffect(() => {
         getData();
@@ -83,6 +105,8 @@ function AirNearTable({cursor}) {
                 Show
             </Button>
             {loading && <Loader scale={1} />}
+            <p>{selectedRecordId}</p>
+            <p>{selectedFieldId}</p>
         </Box>
     );
 }
